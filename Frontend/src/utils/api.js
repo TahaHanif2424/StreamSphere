8b1d6850671a7432c76c1c64b148309df6ea29ee
[mahmed005] <[mahmed.bscs23seecs@seecs.edu.pk]>
2025-05-15 10:29:48 +0500

    Fixed

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -14,14 +14,14 @@ export async function apiFetch(url, opts = {}) {
 
   let res = await fetch(url, options);
 
-  if (res.status === 401) {
-    const refresh = await fetch('/refresh', { method: 'GET', credentials: 'include' });
-    if (refresh.ok) {
-      const { accessToken } = await refresh.json();
-      localStorage.setItem('accessToken', accessToken);
-      options.headers.Authorization = `Bearer ${accessToken}`;
-      res = await fetch(url, options);
-    }
-  }
+  // if (res.status === 401) {
+  //   const refresh = await fetch('/refresh', { method: 'GET', credentials: 'include' });
+  //   if (refresh.ok) {
+  //     const { accessToken } = await refresh.json();
+  //     localStorage.setItem('accessToken', accessToken);
+  //     options.headers.Authorization = `Bearer ${accessToken}`;
+  //     res = await fetch(url, options);
+  //   }
+  // }
   return res;
 }
