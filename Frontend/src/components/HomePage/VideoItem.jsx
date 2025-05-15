e8d34551a4ba390e4ac40eebd22de3d8bc43f58c
[mahmed005] <[mahmed.bscs23seecs@seecs.edu.pk]>
2025-05-15 12:03:13 +0500

    Fixed

--- Frontend/src/components/HomePage/VideoItem.jsx
+++ Frontend/src/components/HomePage/VideoItem.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import defaultChannelPic from '../../public/icon-7797704_640.png';
+import defaultChannelPic from '../../../public/icon-7797704_640.png';
 
 export default function VideoItem({
   _id,
